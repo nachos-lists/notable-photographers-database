ae7c348ca2e2187055c0a2afcbe71bd35b4971f6
Ignacio Dramis <ignacio.dramis@gmail.com>
2025-01-30 14:07:32 -0300

    otro intento con el parser creo

--- scripts/build.js
+++ scripts/build.js
@@ -1,24 +1,36 @@
 
-import esbuild from 'esbuild';
-import esbuildSassPlugin from 'esbuild-sass-plugin';  // Import the SCSS plugin
+// import esbuild from 'esbuild';
+// import esbuildSassPlugin from 'esbuild-sass-plugin';  // Import the SCSS plugin
+
+// esbuild.build({
+  // entryPoints: ['src/index.js'],
+  // bundle: true,
+  // outfile: 'dist/bundle.js',
+  // plugins: [
+    // esbuildSassPlugin()  // Add the SCSS plugin here
+  // ],
+// }).catch(() => process.exit(1));
+
+// import { build } from "esbuild";
+// (async () => {
+  // await build({
+    // entryPoints: ["src/index.js", "src/index.scss"],
+    // bundle: true,
+    // outdir: "dist",
+    // minify: true,
+    // bundle: true,
+    // outdir: "./",
+  // });
+// })();
+
+const esbuild = require('esbuild');
+const esbuildSassPlugin = require('esbuild-sass-plugin');  // Correct import
 
 esbuild.build({
   entryPoints: ['src/index.js'],
   bundle: true,
   outfile: 'dist/bundle.js',
   plugins: [
-    esbuildSassPlugin()  // Add the SCSS plugin here
+    esbuildSassPlugin()  // Use the plugin correctly
   ],
 }).catch(() => process.exit(1));
-
-import { build } from "esbuild";
-(async () => {
-  await build({
-    entryPoints: ["src/index.js", "src/index.scss"],
-    bundle: true,
-    outdir: "dist",
-    minify: true,
-    bundle: true,
-    outdir: "./",
-  });
-})();
