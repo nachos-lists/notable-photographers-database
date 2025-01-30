f119628c04701f69e1be00159e830611729e6884
Ignacio Dramis <ignacio.dramis@gmail.com>
2025-01-30 14:17:49 -0300

    otro intento con el parser creo

--- scripts/build.js
+++ scripts/build.js
@@ -1,28 +1,4 @@
 
-// import esbuild from 'esbuild';
-// import esbuildSassPlugin from 'esbuild-sass-plugin';  // Import the SCSS plugin
-
-// esbuild.build({
-  // entryPoints: ['src/index.js'],
-  // bundle: true,
-  // outfile: 'dist/bundle.js',
-  // plugins: [
-    // esbuildSassPlugin()  // Add the SCSS plugin here
-  // ],
-// }).catch(() => process.exit(1));
-
-// import { build } from "esbuild";
-// (async () => {
-  // await build({
-    // entryPoints: ["src/index.js", "src/index.scss"],
-    // bundle: true,
-    // outdir: "dist",
-    // minify: true,
-    // bundle: true,
-    // outdir: "./",
-  // });
-// })();
-
 const esbuild = require('esbuild');
 const esbuildSassPlugin = require('esbuild-sass-plugin');  // Correct import
 
@@ -34,3 +10,6 @@ esbuild.build({
     esbuildSassPlugin()  // Use the plugin correctly
   ],
 }).catch(() => process.exit(1));
+
+
+
