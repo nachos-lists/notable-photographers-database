80a8b58e24be3299b448c135880fed4976641b54
Ignacio Dramis <ignacio.dramis@gmail.com>
2025-01-30 13:55:03 -0300

    intento resolver el lector o algo así

--- scripts/build.js
+++ scripts/build.js
@@ -1,3 +1,16 @@
+
+import esbuild from 'esbuild';
+import esbuildSassPlugin from 'esbuild-sass-plugin';  // Import the SCSS plugin
+
+esbuild.build({
+  entryPoints: ['src/index.js'],
+  bundle: true,
+  outfile: 'dist/bundle.js',
+  plugins: [
+    esbuildSassPlugin()  // Add the SCSS plugin here
+  ],
+}).catch(() => process.exit(1));
+
 import { build } from "esbuild";
 (async () => {
   await build({
